10eefff5150b25bb4a89f4f1be8a52d2d476c3f6
lzxm160 <lzxm160@sina.com>
2020-02-21 17:46:38 +0800

    x

--- src/interface/TWIoTeXStaking.cpp
+++ src/interface/TWIoTeXStaking.cpp
@@ -16,14 +16,14 @@ using namespace TW;
 
 TWData* _Nonnull TWIoTeXStakingCreate(TWData* _Nonnull candidate, TWData* _Nonnull amount,uint32_t duration, bool autoStake,TWData* _Nonnull payload) {
     auto stakeAction = IoTeX::Proto::StakeCreate();
-    auto c=hex(*candidate)
-    stakeAction.set_candidatename(c); 
-    auto a=hex(*amount)
-    stakeAction.set_stakedamount(a);
+    auto c = dataFromTWData(candidate);
+    stakeAction.set_candidatename(hex(*c)); 
+    auto a = dataFromTWData(amount);
+    stakeAction.set_stakedamount(hex(*a));
     stakeAction.set_stakedduration(duration); 
     stakeAction.set_autostake(autoStake);
-    auto p=hex(*payload)
-    stakeAction.set_payload(p);
+    auto p = dataFromTWData(payload);
+    stakeAction.set_payload(hex(*p));
     auto s = stakeAction.SerializeAsString();
     auto actionHex = hex(s.begin(), s.end()); 
     return TWDataCreateWithHexString(actionHex);
@@ -33,8 +33,8 @@ TWData* _Nonnull TWIoTeXStakingCreate(TWData* _Nonnull candidate, TWData* _Nonnu
 TWData* _Nonnull TWIoTeXStakingReclaim(uint64_t index, TWData* _Nonnull payload) {
     auto reclaimAction = IoTeX::Proto::StakeReclaim();
     reclaimAction.set_bucketindex(index);
-    auto p = hex(*payload)
-    reclaimAction.set_payload(p);
+    auto p = dataFromTWData(payload);
+    reclaimAction.set_payload(hex(*p));
     auto s = reclaimAction.SerializeAsString();
     auto actionHex = hex(s.begin(), s.end());
     return TWDataCreateWithHexString(actionHex);
@@ -44,10 +44,10 @@ TWData* _Nonnull TWIoTeXStakingReclaim(uint64_t index, TWData* _Nonnull payload)
 TWData* _Nonnull TWIoTeXStakingAddDeposit(uint64_t index, TWData* _Nonnull amount,TWData* _Nonnull payload) {
     auto addDepositAction = IoTeX::Proto::StakeAddDeposit();
     addDepositAction.set_bucketindex(index);
-    auto a = hex(*amount)
-    addDepositAction.set_amount(a);
-    auto p = hex(*payload)
-    addDepositAction.set_payload(p);
+    auto a = dataFromTWData(amount);
+    addDepositAction.set_amount(hex(*a));
+    auto p = dataFromTWData(payload);
+    addDepositAction.set_payload(hex(*p));
     auto s = addDepositAction.SerializeAsString();
     auto actionHex = hex(s.begin(), s.end());
     return TWDataCreateWithHexString(actionHex);
@@ -59,8 +59,8 @@ TWData* _Nonnull TWIoTeXStakingRestake(uint64_t index, uint32_t duration,bool au
     restakeAction.set_bucketindex(index);
     restakeAction.set_stakedduration(duration);
     restakeAction.set_autostake(autoStake);
-    auto p = hex(*payload)
-    restakeAction.set_payload(p);
+    auto p = dataFromTWData(payload);
+    restakeAction.set_payload(hex(*p));
     auto s = restakeAction.SerializeAsString();
     auto actionHex = hex(s.begin(), s.end());
     return TWDataCreateWithHexString(actionHex);
@@ -70,11 +70,15 @@ TWData* _Nonnull TWIoTeXStakingRestake(uint64_t index, uint32_t duration,bool au
 TWData* _Nonnull TWIoTeXStakingMove(uint64_t index, TWData* _Nonnull name, TWData* _Nonnull payload) {
     auto moveAction = IoTeX::Proto::StakeMove();
     moveAction.set_bucketindex(index);
-    auto n = hex(*name)
-    moveAction.set_name(n);
-    auto p = hex(*payload)
-    moveAction.set_payload(p);
+    auto n = dataFromTWData(name);
+    moveAction.set_name(hex(*n));
+    auto p = dataFromTWData(payload);
+    moveAction.set_payload(hex(*p));
     auto s = moveAction.SerializeAsString();
     auto actionHex = hex(s.begin(), s.end());
     return TWDataCreateWithHexString(actionHex);
+}
+
+inline std::vector<uint8_t>* dataFromTWData(TWData* data) {
+    return const_cast<std::vector<uint8_t>*>(reinterpret_cast<const std::vector<uint8_t>*>(data));
 }
