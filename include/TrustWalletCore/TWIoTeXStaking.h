3a343390e5d7664cc8dfd3de9a0f6100d5f7bbd1
lzxm160 <lzxm160@sina.com>
2020-02-21 18:39:39 +0800

    x

--- include/TrustWalletCore/TWIoTeXStaking.h
+++ include/TrustWalletCore/TWIoTeXStaking.h
@@ -17,22 +17,22 @@ struct TWIoTeXStaking;
 
 /// Function to generate Stake message
 TW_EXPORT_STATIC_METHOD
-TWData* _Nonnull TWIoTeXStakingCreate(TWData* _Nonnull candidate, uint64_t duration, bool nonDecay, TWData* _Nonnull data);
+TWData* _Nonnull TWIoTeXStakingCreate(TWData* _Nonnull candidate, TWData* _Nonnull amount,uint32_t duration, bool autoStake, TWData* _Nonnull payload);
 
 /// Function to generate Unstake message
 TW_EXPORT_STATIC_METHOD
-TWData* _Nonnull TWIoTeXStakingUnstake(uint64_t pyggIndex, TWData* _Nonnull data);
+TWData* _Nonnull TWIoTeXStakingReclaim(uint64_t index, TWData* _Nonnull payload);
 
 /// Function to generate Withdraw message
 TW_EXPORT_STATIC_METHOD
-TWData* _Nonnull TWIoTeXStakingWithdraw(uint64_t pyggIndex, TWData* _Nonnull data);
+TWData* _Nonnull TWIoTeXStakingAddDeposit(uint64_t index, TWData* _Nonnull amount,TWData* _Nonnull payload);
 
 /// Function to generate AddStake message
 TW_EXPORT_STATIC_METHOD
-TWData* _Nonnull TWIoTeXStakingAddStake(uint64_t pyggIndex, TWData* _Nonnull data);
+TWData* _Nonnull TWIoTeXStakingRestake(uint64_t index, uint32_t duration, bool autoStake,TWData* _Nonnull payload);
 
 /// Function to generate MoveStake message
 TW_EXPORT_STATIC_METHOD
-TWData* _Nonnull TWIoTeXStakingMoveStake(uint64_t pyggIndex, TWData* _Nonnull candidate, TWData* _Nonnull data);
+TWData* _Nonnull TWIoTeXStakingMove(uint64_t index, TWData* _Nonnull name,TWData* _Nonnull payload);
 
 TW_EXTERN_C_END
