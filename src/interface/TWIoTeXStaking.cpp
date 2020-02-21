94eb73e8f26a8cb885de1a41ef138851d1b52120
lzxm160 <lzxm160@sina.com>
2020-02-21 16:08:05 +0800

    x

--- src/interface/TWIoTeXStaking.cpp
+++ src/interface/TWIoTeXStaking.cpp
@@ -10,77 +10,83 @@
 
 #include "Data.h"
 #include "uint256.h"
+#include "proto/IoTeX.pb.h"
 
 using namespace TW;
 
-TWData* _Nonnull TWIoTeXStakingStake(TWData* _Nonnull candidate, uint64_t stakeDuration, bool nonDecay, TWData* _Nonnull data) {
-    TWEthereumAbiFunction* func = TWEthereumAbiEncoderBuildFunction(TWStringCreateWithUTF8Bytes("createPygg"));
-    assert(func != nullptr);
-
-    TWEthereumAbiFunctionAddParamBytesFix(func, 12, candidate, false);
-    auto v = store(uint256_t(stakeDuration));
-    TWEthereumAbiFunctionAddParamUInt256(func, &v, false);
-    TWEthereumAbiFunctionAddParamBool(func, nonDecay, false);
-    TWEthereumAbiFunctionAddParamBytes(func, data, false);
-
-    auto ret = TWEthereumAbiEncoderEncode(func);
-    TWEthereumAbiEncoderDeleteFunction(func);
-    return ret;
+TWData* _Nonnull TWIoTeXStakingCreate(TWData* _Nonnull candidate, TWData* _Nonnull amount,
+                                      uint32_t duration, bool autoStake,
+                                      TWData* _Nonnull payload) {
+    auto stakeAction = IoTeX::Proto::StakeCreate();
+    stakeAction.set_candidatename(candidate); 
+    stakeAction.set_stakedamount(amount);
+    stakeAction.set_stakedduration(duration); 
+    stakeAction.set_autostake(autoStake);
+    stakeAction.set_payload(data);
+    return TWDataCreateWithHexString(stakeAction.SerializeAsString());
 }
 
 /// Function to generate Unstake message
 TWData* _Nonnull TWIoTeXStakingUnstake(uint64_t pyggIndex, TWData* _Nonnull data) {
-    TWEthereumAbiFunction* func = TWEthereumAbiEncoderBuildFunction(TWStringCreateWithUTF8Bytes("unstake"));
-    assert(func != nullptr);
+    // TWEthereumAbiFunction* func =
+    // TWEthereumAbiEncoderBuildFunction(TWStringCreateWithUTF8Bytes("unstake"));
+    // assert(func != nullptr);
 
-    auto v = store(uint256_t(pyggIndex));
-    TWEthereumAbiFunctionAddParamUInt256(func, &v, false);
-    TWEthereumAbiFunctionAddParamBytes(func, data, false);
+    // auto v = store(uint256_t(pyggIndex));
+    // TWEthereumAbiFunctionAddParamUInt256(func, &v, false);
+    // TWEthereumAbiFunctionAddParamBytes(func, data, false);
 
-    auto ret = TWEthereumAbiEncoderEncode(func);
-    TWEthereumAbiEncoderDeleteFunction(func);
-    return ret;
+    // auto ret = TWEthereumAbiEncoderEncode(func);
+    // TWEthereumAbiEncoderDeleteFunction(func);
+    // return ret;
+    return 0;
 }
 
 /// Function to generate Withdraw message
 TWData* _Nonnull TWIoTeXStakingWithdraw(uint64_t pyggIndex, TWData* _Nonnull data) {
-    TWEthereumAbiFunction* func = TWEthereumAbiEncoderBuildFunction(TWStringCreateWithUTF8Bytes("withdraw"));
-    assert(func != nullptr);
+    // TWEthereumAbiFunction* func =
+    // TWEthereumAbiEncoderBuildFunction(TWStringCreateWithUTF8Bytes("withdraw"));
+    // assert(func != nullptr);
 
-    auto v = store(uint256_t(pyggIndex));
-    TWEthereumAbiFunctionAddParamUInt256(func, &v, false);
-    TWEthereumAbiFunctionAddParamBytes(func, data, false);
+    // auto v = store(uint256_t(pyggIndex));
+    // TWEthereumAbiFunctionAddParamUInt256(func, &v, false);
+    // TWEthereumAbiFunctionAddParamBytes(func, data, false);
 
-    auto ret = TWEthereumAbiEncoderEncode(func);
-    TWEthereumAbiEncoderDeleteFunction(func);
-    return ret;
+    // auto ret = TWEthereumAbiEncoderEncode(func);
+    // TWEthereumAbiEncoderDeleteFunction(func);
+    // return ret;
+    return 0;
 }
 
 /// Function to generate AddStake message
 TWData* _Nonnull TWIoTeXStakingAddStake(uint64_t pyggIndex, TWData* _Nonnull data) {
-    TWEthereumAbiFunction* func = TWEthereumAbiEncoderBuildFunction(TWStringCreateWithUTF8Bytes("storeToPygg"));
-    assert(func != nullptr);
+    // TWEthereumAbiFunction* func =
+    // TWEthereumAbiEncoderBuildFunction(TWStringCreateWithUTF8Bytes("storeToPygg"));
+    // assert(func != nullptr);
 
-    auto v = store(uint256_t(pyggIndex));
-    TWEthereumAbiFunctionAddParamUInt256(func, &v, false);
-    TWEthereumAbiFunctionAddParamBytes(func, data, false);
+    // auto v = store(uint256_t(pyggIndex));
+    // TWEthereumAbiFunctionAddParamUInt256(func, &v, false);
+    // TWEthereumAbiFunctionAddParamBytes(func, data, false);
 
-    auto ret = TWEthereumAbiEncoderEncode(func);
-    TWEthereumAbiEncoderDeleteFunction(func);
-    return ret;
+    // auto ret = TWEthereumAbiEncoderEncode(func);
+    // TWEthereumAbiEncoderDeleteFunction(func);
+    // return ret;
+    return 0;
 }
 
 /// Function to generate MoveStake message
 TWData* _Nonnull TWIoTeXStakingMoveStake(uint64_t pyggIndex, TWData* _Nonnull candidate, TWData* _Nonnull data) {
-    TWEthereumAbiFunction* func = TWEthereumAbiEncoderBuildFunction(TWStringCreateWithUTF8Bytes("revote"));
-    assert(func != nullptr);
+    // TWEthereumAbiFunction* func =
+    // TWEthereumAbiEncoderBuildFunction(TWStringCreateWithUTF8Bytes("revote"));
+    // assert(func != nullptr);
 
-    auto v = store(uint256_t(pyggIndex));
-    TWEthereumAbiFunctionAddParamUInt256(func, &v, false);
-    TWEthereumAbiFunctionAddParamBytesFix(func, 12, candidate, false);
-    TWEthereumAbiFunctionAddParamBytes(func, data, false);
+    // auto v = store(uint256_t(pyggIndex));
+    // TWEthereumAbiFunctionAddParamUInt256(func, &v, false);
+    // TWEthereumAbiFunctionAddParamBytesFix(func, 12, candidate, false);
+    // TWEthereumAbiFunctionAddParamBytes(func, data, false);
 
-    auto ret = TWEthereumAbiEncoderEncode(func);
-    TWEthereumAbiEncoderDeleteFunction(func);
-    return ret;
+    // auto ret = TWEthereumAbiEncoderEncode(func);
+    // TWEthereumAbiEncoderDeleteFunction(func);
+    // return ret;
+    return 0;
 }
