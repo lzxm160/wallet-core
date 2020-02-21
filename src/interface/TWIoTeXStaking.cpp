43d450873ec7b577e2763cf39603adfed2064889
lzxm160 <lzxm160@sina.com>
2020-02-21 17:16:28 +0800

    x

--- src/interface/TWIoTeXStaking.cpp
+++ src/interface/TWIoTeXStaking.cpp
@@ -16,11 +16,11 @@ using namespace TW;
 
 TWData* _Nonnull TWIoTeXStakingCreate(TWData* _Nonnull candidate, TWData* _Nonnull amount,uint32_t duration, bool autoStake,TWData* _Nonnull payload) {
     auto stakeAction = IoTeX::Proto::StakeCreate();
-    stakeAction.set_candidatename(candidate); 
-    stakeAction.set_stakedamount(amount);
+    stakeAction.set_candidatename(hex(*candidate)); 
+    stakeAction.set_stakedamount(hex(*amount));
     stakeAction.set_stakedduration(duration); 
     stakeAction.set_autostake(autoStake);
-    stakeAction.set_payload(data);
+    stakeAction.set_payload(hex(*payload));
     auto s = stakeAction.SerializeAsString();
     auto actionHex = hex(s.begin(), s.end()); 
     return TWDataCreateWithHexString(actionHex);
@@ -30,17 +30,21 @@ TWData* _Nonnull TWIoTeXStakingCreate(TWData* _Nonnull candidate, TWData* _Nonnu
 TWData* _Nonnull TWIoTeXStakingReclaim(uint64_t index, TWData* _Nonnull payload) {
     auto reclaimAction = IoTeX::Proto::StakeReclaim();
     reclaimAction.set_bucketindex(index);
-    reclaimAction.set_payload(payload);
-    return TWDataCreateWithHexString(reclaimAction.SerializeAsString());
+    reclaimAction.set_payload(hex(*payload));
+    auto s = reclaimAction.SerializeAsString();
+    auto actionHex = hex(s.begin(), s.end());
+    return TWDataCreateWithHexString(actionHex);
 }
 
 /// Function to generate AddDeposit message
 TWData* _Nonnull TWIoTeXStakingAddDeposit(uint64_t index, TWData* _Nonnull amount,TWData* _Nonnull payload) {
     auto addDepositAction = IoTeX::Proto::StakeAddDeposit();
     addDepositAction.set_bucketindex(index);
-    addDepositAction.set_amount(amount);
-    addDepositAction.set_payload(payload);
-    return TWDataCreateWithHexString(addDepositAction.SerializeAsString());
+    addDepositAction.set_amount(hex(*amount));
+    addDepositAction.set_payload(hex(*payload));
+    auto s = addDepositAction.SerializeAsString();
+    auto actionHex = hex(s.begin(), s.end());
+    return TWDataCreateWithHexString(actionHex);
 }
 
 /// Function to generate Restake message
@@ -49,15 +53,19 @@ TWData* _Nonnull TWIoTeXStakingRestake(uint64_t index, uint32_t duration,bool au
     restakeAction.set_bucketindex(index);
     restakeAction.set_stakedduration(duration);
     restakeAction.set_autostake(autoStake);
-    restakeAction.set_payload(payload);
-    return TWDataCreateWithHexString(restakeAction.SerializeAsString());
+    restakeAction.set_payload(hex(*payload));
+    auto s = restakeAction.SerializeAsString();
+    auto actionHex = hex(s.begin(), s.end());
+    return TWDataCreateWithHexString(actionHex);
 }
 
 /// Function to generate Move message
 TWData* _Nonnull TWIoTeXStakingMove(uint64_t index, TWData* _Nonnull name, TWData* _Nonnull payload) {
     auto moveAction = IoTeX::Proto::StakeMove();
     moveAction.set_bucketindex(index);
-    moveAction.set_name(name);
-    moveAction.set_payload(payload);
-    return TWDataCreateWithHexString(moveAction.SerializeAsString());
+    moveAction.set_name(hex(*name));
+    moveAction.set_payload(hex(*payload));
+    auto s = moveAction.SerializeAsString();
+    auto actionHex = hex(s.begin(), s.end());
+    return TWDataCreateWithHexString(actionHex);
 }
