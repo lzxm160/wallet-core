de0824121dccd2c3ac3083700fdb3d1409112247
lzxm160 <lzxm160@sina.com>
2020-04-04 19:41:15 +0800

    x

--- src/IoTeX/Staking.cpp
+++ src/IoTeX/Staking.cpp
@@ -40,12 +40,12 @@ std::string stringFromData(const Data& data) {
 }
 
 const Data& dataFromString(const std::string& d) {
-    Data data;
-    std::copy(d.c_str(), d.c_str() + d.length(), back_inserter(data));
-    return data;
+    Data* data = new Data;
+    std::copy(d.c_str(), d.c_str() + d.length(), back_inserter(*data));
+    return *data;
 
-    Data data(d.begin(), d.end());
-    return data;
+    // Data data(d.begin(), d.end());
+    // return data;
 }
 const Data& stakingCreate(const Data& candidate, const Data& amount, uint32_t duration,
                           bool autoStake, const Data& payload) {
