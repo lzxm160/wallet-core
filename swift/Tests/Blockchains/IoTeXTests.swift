e42a8460cc38413a518f40350b6e4a253245c482
lzxm160 <lzxm160@sina.com>
2020-04-06 18:38:58 +0800

    x

--- swift/Tests/Blockchains/IoTeXTests.swift
+++ swift/Tests/Blockchains/IoTeXTests.swift
@@ -37,7 +37,7 @@ class IoTeXTests: XCTestCase {
             $0.gasPrice = "10"
             $0.privateKey = Data(hexString: "cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1")!
         }
-        input.staking.stakeCreate = IoTeXStaking.stakingCreate.with {
+        input.staking.stakeCreate = IoTeXStaking.StakeCreate.with {
             $0.candidate = "io19d0p3ah4g8ww9d7kcxfq87yxe7fnr8rpth5shj"
             $0.amount = "100"
             $0.duration = 10000
@@ -58,7 +58,7 @@ class IoTeXTests: XCTestCase {
             $0.gasPrice = "10"
             $0.privateKey = Data(hexString: "cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1")!
         }
-        input.staking.stakeAddDeposit = IoTeXStaking.stakingAddDeposit.with {
+        input.staking.stakeAddDeposit = IoTeXStaking.StakeAddDeposit.with {
             $0.index = 10
             $0.amount = "100"
             $0.payload = "payload"
@@ -77,7 +77,7 @@ class IoTeXTests: XCTestCase {
             $0.gasPrice = "10"
             $0.privateKey = Data(hexString: "cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1")!
         }
-        input.staking.stakeUnstake = IoTeXStaking.stakingUnstake.with {
+        input.staking.stakeUnstake = IoTeXStaking.StakeUnstake.with {
             $0.index = 10
             $0.payload = "payload"
         }
@@ -95,7 +95,7 @@ class IoTeXTests: XCTestCase {
             $0.gasPrice = "10"
             $0.privateKey = Data(hexString: "cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1")!
         }
-        input.staking.stakeWithdraw = IoTeXStaking.stakingWithdraw.with {
+        input.staking.stakeWithdraw = IoTeXStaking.StakeWithdraw.with {
             $0.index = 10
             $0.payload = "payload"
         }
@@ -113,7 +113,7 @@ class IoTeXTests: XCTestCase {
             $0.gasPrice = "10"
             $0.privateKey = Data(hexString: "cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1")!
         }
-        input.staking.stakeRestake = IoTeXStaking.stakingRestake.with {
+        input.staking.stakeRestake = IoTeXStaking.StakeRestake.with {
             $0.index = 10
             $0.duration = 1000
             $0.autoStake = true
@@ -134,7 +134,7 @@ class IoTeXTests: XCTestCase {
             $0.gasPrice = "10"
             $0.privateKey = Data(hexString: "cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1")!
         }
-        input.staking.stakeChangeCandidate = IoTeXStaking.stakingChangeCandidate.with {
+        input.staking.stakeChangeCandidate = IoTeXStaking.StakeChangeCandidate.with {
             $0.index = 10    
             $0.candidate = "io1xpq62aw85uqzrccg9y5hnryv8ld2nkpycc3gza"
             $0.payload = "payload"
@@ -153,7 +153,7 @@ class IoTeXTests: XCTestCase {
             $0.gasPrice = "10"
             $0.privateKey = Data(hexString: "cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1")!
         }
-        input.staking.stakeTransferOwnership = IoTeXStaking.stakingTransfer.with {
+        input.staking.stakeTransferOwnership = IoTeXStaking.StakeTransferOwnership.with {
             $0.index = 10    
             $0.voterAddress = "io1xpq62aw85uqzrccg9y5hnryv8ld2nkpycc3gza"
             $0.payload = "payload"
@@ -172,7 +172,7 @@ class IoTeXTests: XCTestCase {
             $0.gasPrice = "1000"
             $0.privateKey = Data(hexString: "cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1")!
         }
-        input.staking.candidateRegister = IoTeXStaking.candidateRegister.with {
+        input.staking.candidateRegister = IoTeXStaking.CandidateRegister.with {
             $0.name = "test"
             $0.operatorAddress = "io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he"   
             $0.rewardAddress = "io13sj9mzpewn25ymheukte4v39hvjdtrfp00mlyv"
@@ -196,7 +196,7 @@ class IoTeXTests: XCTestCase {
             $0.gasPrice = "10"
             $0.privateKey = Data(hexString: "cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1")!
         }
-        input.staking.candidateUpdate = IoTeXStaking.candidateUpdate.with {
+        input.staking.candidateUpdate = IoTeXStaking.CandidateUpdate.with {
             $0.name = "test"
             $0.operatorAddress = "io1cl6rl2ev5dfa988qmgzg2x4hfazmp9vn2g66ng"   
             $0.rewardAddress = "io1juvx5g063eu4ts832nukp4vgcwk2gnc5cu9ayd"
@@ -207,83 +207,3 @@ class IoTeXTests: XCTestCase {
         XCTAssertEqual(output.hash.hexString, "ca1a28f0e9a58ffc67037cc75066dbdd8e024aa2b2e416e4d6ce16c3d86282e5")
     }
 }
-
-    // func stakingInput() -> IoTeXSigningInput {
-    //     let input = IoTeXSigningInput.with {
-    //         $0.version = 1
-    //         $0.nonce = 123
-    //         $0.gasLimit = 888
-    //         $0.gasPrice = "999"
-    //         $0.staking = IoTeXStaking.with {
-    //             $0.amount = "456"
-    //             $0.contract = "io1xpq62aw85uqzrccg9y5hnryv8ld2nkpycc3gza"
-    //         }
-    //         $0.privateKey = Data(hexString: "0806c458b262edd333a191e92f561aff338211ee3e18ab315a074a2d82aa343f")!
-    //     }
-    //     return input
-    // }
-
-
-
-    // func testStake() {
-        //     // candidate name is a string
-        //     let candidate = "\u{01}\u{02}\u{03}\u{04}\u{05}\u{06}\u{07}\u{08}\t\n\u{0B}\u{0C}"
-        //     var input = stakingInput()
-        //     input.staking.stake = IoTeXStaking.Stake.with {
-        //         $0.candidate = candidate
-        //         $0.duration = 1001
-        //         $0.nonDecay = true
-        //         $0.data = "this is a test".data(using: .utf8)!
-        //     }
-        //     let output: IoTeXSigningOutput = AnySigner.sign(input: input, coin: .ioTeX)
-
-        //     XCTAssertEqual(output.encoded.hexString, "0a86020801107b18f806220339393962f7010a033435361229696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611ac40107c35fc00102030405060708090a0b0c000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003e900000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000e74686973206973206120746573740000000000000000000000000000000000001241044e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34fbeb71270d4bad3d648d91a41a558bc9a4bfba920242ccd4d5c5da363ec534d4dd5eb67f88e9db7aaad5c50ad62dfe298c0e54e311ebba045f48cea1136e42a123a8e6b03d3e6ed82d4ec2b9401")
-        //     XCTAssertEqual(output.hash.hexString, "41b1f8be5f6b884c06556fba2611716e8e514b507f5a653fc02ac50ba13fbd6c")
-        // }
-
-        // func testUnstake() {
-        //     var input = stakingInput()
-        //     input.staking.unstake = IoTeXStaking.Unstake.with {
-        //         $0.piggyIndex = 1001
-        //     }
-        //     let output: IoTeXSigningOutput = AnySigner.sign(input: input, coin: .ioTeX)
-
-        //     XCTAssertEqual(output.encoded.hexString, "0aa5010801107b18f80622033939396296010a033435361229696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611a64c8fd6ed000000000000000000000000000000000000000000000000000000000000003e9000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000001241044e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34fbeb71270d4bad3d648d91a41eeb7cb3fa7ec22a61156753d569b3f4da3c74c3c7e2f148b1a43e11d220cac5d164663ff6c785439679b088de9d7f2437545f007ca9cda4b2f5327d2c6eda5aa01")
-        //     XCTAssertEqual(output.hash.hexString, "b93a2874a72ce4eb8a41a20c209cf3fd188671ed8be8239a57960cbed887e962")
-        // }
-
-        // func testWithdraw() {
-        //     var input = stakingInput()
-        //     input.staking.withdraw = IoTeXStaking.Withdraw.with {
-        //         $0.piggyIndex = 1001
-        //     }
-        //     let output: IoTeXSigningOutput = AnySigner.sign(input: input, coin: .ioTeX)
-
-        //    XCTAssertEqual(output.encoded.hexString, "0aa5010801107b18f80622033939396296010a033435361229696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611a64030ba25d00000000000000000000000000000000000000000000000000000000000003e9000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000001241044e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34fbeb71270d4bad3d648d91a41903c79d042f6b1c05446ececb3d760ca154c539b5787e66135cd3db77638294d18dbbbbcb0de8b9a393cc7c0448cf246898e4343a2a51666e21e738ee6d8a6f700")
-        //     XCTAssertEqual(output.hash.hexString, "2b2657247a72cb262de214b4e793c7a01fa2139fd5d12a46d43c24f87f9e2396")
-        // }
-
-        // func testAddStake() {
-        //     var input = stakingInput()
-        //     input.staking.addStake = IoTeXStaking.AddStake.with {
-        //         $0.piggyIndex = 1001
-        //     }
-        //     let output: IoTeXSigningOutput = AnySigner.sign(input: input, coin: .ioTeX)
-
-        //    XCTAssertEqual(output.encoded.hexString, "0aa5010801107b18f80622033939396296010a033435361229696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611a646e7b301700000000000000000000000000000000000000000000000000000000000003e9000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000001241044e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34fbeb71270d4bad3d648d91a410f0832bb7a48c9e468c0bcb2c4a35a202f9519a63c4d6474b48087ab9dc33aea18127940d5cda43710cd874cbdf7a7b26efc9c04236e14dfb4d9b6f7095b0b6c01")
-        //     XCTAssertEqual(output.hash.hexString, "c71058812a5febe5cdcdaf9499ba0b2c895f88d1acd3203e5097b307c2a5f1d1")
-        // }
-
-        // func testMoveStake() {
-        //     // candidate name is a string
-        //     let candidate = "\u{01}\u{02}\u{03}\u{04}\u{05}\u{06}\u{07}\u{08}\t\n\u{0B}\u{0C}"
-        //     var input = stakingInput()
-        //     input.staking.moveStake = IoTeXStaking.MoveStake.with {
-        //         $0.piggyIndex = 1001
-        //         $0.candidate = candidate
-        //     }
-        //     let output: IoTeXSigningOutput = AnySigner.sign(input: input, coin: .ioTeX)
-
-        //    XCTAssertEqual(output.encoded.hexString, "0ac6010801107b18f806220339393962b7010a033435361229696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611a8401d3e41fd200000000000000000000000000000000000000000000000000000000000003e90102030405060708090a0b0c0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000001241044e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34fbeb71270d4bad3d648d91a4118b71944993d1cd5362379ff64e892024a75ce697b2755cc9bbfcac24482a19b1ec2951bec7461ef9eb0723f803987cb87e3c3afb340006cd5b413c1fac10d7c01")
-        //     XCTAssertEqual(output.hash.hexString, "33290ded342efaebf795855be73d34cbac149a2415ff9558de10303e6126f30d")
-        // }
